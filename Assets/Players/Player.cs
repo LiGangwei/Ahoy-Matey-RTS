389cb7005fff1c2aa9d5a95f82501d484818fcc8
Li Gangwei <michele@n1164844147.netvigator.com>
2017-05-31 02:06:10 +0800

    NetworkBehaviour isLocalPlayer

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLocalPlayer) {
+			transform.position += new Vector3(CrossPlatformInputManager.GetAxis("Horizontal")*speed*Time.deltaTime, CrossPlatformInputManager.GetAxis("Vertical")*speed*Time.deltaTime, 0f);
+		}
+	}
+}
